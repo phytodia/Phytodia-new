ea9eafdd7191daee7249852126ff4726937d8060
thomas hunckler <th@phytodia.com>
2024-02-23 15:08:53 +0100

    change

--- app/javascript/controllers/saponification_controller.js
+++ app/javascript/controllers/saponification_controller.js
@@ -5,11 +5,15 @@ export default class extends Controller {
   static targets = ["ingredient","ingredientsJson"]
   connect() {
     console.log("sapo")
-    const ingredients = JSON.parse(this.ingredientsJsonTarget.dataset.ingredients)
-    console.log(ingredients["Abyssinian Oil"])
   }
   getIngredient(event){
-    let ingredient = event.currentTarget.value
-    console.log(ingredient)
+      let ingredient = event.currentTarget.value
+      const ingredients = JSON.parse(this.ingredientsJsonTarget.dataset.ingredients)
+      //console.log(ingredients["Abyssinian Oil"])
+      let inputs = Array.from(document.querySelector("#caracteristiques").querySelectorAll("input"))
+      inputs.forEach((element) => {
+        element.value = ingredients[ingredient][element.name]
+      })
   }
+
 }
