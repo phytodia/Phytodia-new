ef29ba393c00c92dca179132b027db741faf635c
thomas hunckler <th@phytodia.com>
2024-03-01 15:47:44 +0100

    change

--- app/javascript/controllers/saponification_controller.js
+++ app/javascript/controllers/saponification_controller.js
@@ -2,9 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="saponification"
 export default class extends Controller {
-  static targets = ["ingredient","ingredientsJson"]
+  static targets = ["ingredient","ingredientsJson","ingredientTable"]
   connect() {
     console.log("sapo")
+
   }
   getIngredient(event){
       let ingredient = event.currentTarget.value
@@ -14,6 +15,20 @@ export default class extends Controller {
       inputs.forEach((element) => {
         element.value = ingredients[ingredient][element.name]
       })
+  }
+  doubleClick(event){
+    //alert("Gogole")
+    let newTd = '<td><input type="text"></td><td><input type="number"></td><td><input type="number"></td>'
+    let newTr = document.createElement('tr')
+    newTr.innerHTML = newTd
+    this.ingredientTableTarget.appendChild(newTr)
+    this.ingredientTableTarget.lastChild.getElementsByTagName("input")[0].value = event.currentTarget.value
+
+    //let newtr = document.createElement(
+    //  "<tr><td><input type="text" data-saponification-target="ingredientTable"></td><td><input type="text"></td><td><input type="text"></td></tr>"
+    //)
+
+    //this.ingredientTableTarget.value = event.currentTarget.value
   }
 
 }
