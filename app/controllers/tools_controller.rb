6368eb38645edc060c1aa4c831f6430f58b32ee0
thomas hunckler <th@phytodia.com>
2024-10-24 16:41:19 +0200

    change

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -2,7 +2,7 @@ class ToolsController < ApplicationController
   def saponification
     @ingredients = YAML.load_file("#{Rails.root.to_s}/db/data/saponification.yml")
     @json = @ingredients.to_json
-    @labels = ["abc","def","ghi","jkl","mno"]
+    @labels = [""]
     @series = [50, 50, 50, 50, 50, 20]
     #data = Applicant.includes(:job).group('status').count
     #@labels = ["Test1","Test2"]
