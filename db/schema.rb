f718d7e319c0d2449f0a813225d2dad81e05c86c
thomas hunckler <th@phytodia.com>
2024-11-29 14:42:55 +0100

    change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_29_082943) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_29_132245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_29_082943) do
     t.integer "stearic"
     t.integer "ricinoleic"
     t.integer "oleic"
-    t.integer "linoleic"
+    t.integer "Linoeic"
     t.integer "linolenic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
